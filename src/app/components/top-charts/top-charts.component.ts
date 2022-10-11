679e2c3b63d6a8981a8721783287c31df90fd0db
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-10-11 19:26:53 +0100

    created top charts card

--- src/app/components/top-charts/top-charts.component.ts
+++ src/app/components/top-charts/top-charts.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-charts',
   templateUrl: './top-charts.component.html',
-  styleUrls: ['./top-charts.component.css']
+  styleUrls: ['./top-charts.component.css'],
 })
 export class TopChartsComponent implements OnInit {
+  toggle: boolean = false;
+  val: string = 'none';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ontoggle() {
+    this.toggle = !this.toggle;
+    this.val = this.toggle ? '#FACD66' : 'none';
   }
-
 }
