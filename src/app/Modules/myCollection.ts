9e03e9cf92b965303de7ff444cfb904dc3b78e06
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-10-21 13:15:05 +0100

    created class and dummy data for collections

---
+++ src/app/Modules/myCollection.ts
@@ -0,0 +1,22 @@
+import { collections } from './collections';
+
+export const myCollection: collections[] = [
+  {
+    title: 'Limits',
+    artist: 'John Watts',
+    likes: '2.3',
+    album_cover: '../../assets/SVG/album_art/album_art (3).svg',
+  },
+  {
+    title: 'Unlimited',
+    artist: 'Mega Watts',
+    likes: '1.3',
+    album_cover: '../../assets/SVG/album_art/album_art (4).svg',
+  },
+  {
+    title: 'The Red Balloon Project',
+    artist: 'Mohd Izan',
+    likes: '0.5',
+    album_cover: '../../assets/SVG/album_art/album_art (6).svg',
+  },
+];
--- src/app/pages/collections/collections.component.ts
+++ src/app/pages/collections/collections.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { myCollection } from 'src/app/Modules/myCollection';
+import { collections } from 'src/app/Modules/collections';
 
 @Component({
   selector: 'app-collections',
   templateUrl: './collections.component.html',
-  styleUrls: ['./collections.component.css']
+  styleUrls: ['./collections.component.css'],
 })
 export class CollectionsComponent implements OnInit {
+  collections: collections[] = myCollection;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
